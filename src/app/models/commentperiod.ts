940dd88ee034ae3b2ab7a01e89588c44d7f45875
Severin Beauvais <severin.beauvais@gmail.com>
2018-11-16 11:25:49 -0800

    PRC-852: model cleanup

--- src/app/models/commentperiod.ts
+++ src/app/models/commentperiod.ts
@@ -2,7 +2,6 @@ export class CommentPeriod {
   _id: string;
   _addedBy: string;
   _application: string;
-  code: string;
   startDate: Date = null;
   endDate: Date = null;
 
@@ -10,7 +9,6 @@ export class CommentPeriod {
     this._id          = obj && obj._id          || null;
     this._addedBy     = obj && obj._addedBy     || null;
     this._application = obj && obj._application || null;
-    this.code         = obj && obj.code         || null;
 
     if (obj && obj.startDate) {
       this.startDate = new Date(obj.startDate);
